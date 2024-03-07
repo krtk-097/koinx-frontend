d5a31a15543429f303085ac8b24f6b4f157b64e9
Kartik <frankyz2190@gmail.com>
2024-03-07 18:22:45 +0530

    completed Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import NavBar from "./components/NavBar.jsx";
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <NavBar />
+    </div>
+  );
 }
+
+export default App;
